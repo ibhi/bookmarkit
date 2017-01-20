6ed5f2c310243b346b909b6944fe32b19c74fe9d
ibhi <ibrahimin2004@gmail.com>
2017-01-20 18:38:12 +0530

    WIP: Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddNoteComponent
   ],
   imports: [
     BrowserModule,
